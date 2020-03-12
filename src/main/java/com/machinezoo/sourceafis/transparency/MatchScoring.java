e8ba59ffba5bd983d859473df8f2e1cb11906778
Robert Vazan <robvcode@gmail.com>
2020-03-12 01:44:44 +0000

    Updated scoring record

--- src/main/java/com/machinezoo/sourceafis/transparency/MatchScoring.java
+++ src/main/java/com/machinezoo/sourceafis/transparency/MatchScoring.java
@@ -7,19 +7,25 @@ import java.util.function.*;
 import com.google.gson.*;
 
 public class MatchScoring {
-	public int matchedMinutiae;
-	public double matchedMinutiaeScore;
-	public double matchedFractionOfProbeMinutiae;
-	public double matchedFractionOfCandidateMinutiae;
-	public double matchedFractionOfAllMinutiaeScore;
-	public int matchedEdges;
-	public double matchedEdgesScore;
-	public int minutiaeWithSeveralEdges;
-	public double minutiaeWithSeveralEdgesScore;
-	public int correctMinutiaTypeCount;
-	public double correctMinutiaTypeScore;
-	public double accurateEdgeLengthScore;
-	public double accurateMinutiaAngleScore;
+	public int minutiaCount;
+	public double minutiaScore;
+	public double minutiaFractionInProbe;
+	public double minutiaFractionInCandidate;
+	public double minutiaFraction;
+	public double minutiaFractionScore;
+	public int supportingEdgeSum;
+	public int edgeCount;
+	public double edgeScore;
+	public int supportedMinutiaCount;
+	public double supportedMinutiaScore;
+	public int minutiaTypeHits;
+	public double minutiaTypeScore;
+	public int distanceErrorSum;
+	public int distanceAccuracySum;
+	public double distanceAccuracyScore;
+	public double angleErrorSum;
+	public double angleAccuracySum;
+	public double angleAccuracyScore;
 	public double totalScore;
 	public double shapedScore;
 	public static MatchScoring parse(Map<String, Supplier<byte[]>> bundle) {
