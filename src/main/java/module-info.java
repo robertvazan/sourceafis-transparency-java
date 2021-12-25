6237970cd449f1cbd00c4ca33a3c5b493195928f
Robert Važan <robert.vazan@tutanota.com>
2021-12-25 16:46:38 +0000

    Fixed dependency list

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,14 @@
 module com.machinezoo.sourceafis.transparency {
 	exports com.machinezoo.sourceafis.transparency;
 	requires com.machinezoo.noexception;
+	/*
+	 * Transitive, because we provide FingerprintTransparency implementation in the API.
+	 */
 	requires transitive com.machinezoo.sourceafis;
 	requires com.fasterxml.jackson.databind;
 	requires com.fasterxml.jackson.dataformat.cbor;
+	requires org.apache.commons.io;
+	requires it.unimi.dsi.fastutil;
 	/*
 	 * Serialization needs reflection access.
 	 */
