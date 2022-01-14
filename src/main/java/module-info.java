9d617eb6e4550bf55666b428643916c1748e0280
Robert Važan <robert.vazan@tutanota.com>
2022-01-14 14:37:48 +0000

    Strategy for supporting multiple MIME types and multiple versions per key name

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,20 +4,19 @@ import com.machinezoo.stagean.*;
 @NoDocs
 @DraftTests("Reach 100% coverage.")
 @DraftApi
+@ApiIssue("Allow keys with the same name that differ in supported MIME types.")
 /*
- * Aside from just dismissing all compatibility completely, there's the option of versioned classes:
+ * This is required to support multiple SourceAFIS versions:
  * 
- * - modified types and keys get version suffix (e.g. 3v14), old types and keys are immediately deprecated
- * - deprecated types and keys are regularly removed when major version of this library is incremented
- * - when unversioned symbol is removed, latest versioned symbol is copied to it and the versioned symbol is deprecated
- * - key parsing requires version as parameter, key objects know which versions they apply to
- * - keys with the same name are tolerated as long as they apply to different versions
- * - TransparencyBuffer gets SourceAFIS version from VersionKey
- * - visualizations are not versioned, they instead support several key versions as dependencies
+ * - Key parsing requires version as parameter.
+ * - TransparencyBuffer gets SourceAFIS version from VersionKey.
+ * - Modified types and keys get version suffix (e.g. 3v14). Old types and keys are immediately deprecated.
+ * - Deprecated types and keys are regularly removed when major version of this library is incremented.
+ * - When unversioned symbol is removed, latest versioned symbol is copied to it and the versioned symbol is deprecated.
  * 
  * Unstable 0.x releases do not need to bother with compatibility. This can be delayed until release 1.0.
  */
-@ApiIssue("Figure out how to provide compatibility over a range of recent SourceAFIS versions.")
+@ApiIssue("Allow keys with the same name that differ in SourceAFIS versions they apply to.")
 module com.machinezoo.sourceafis.transparency {
 	exports com.machinezoo.sourceafis.transparency;
 	exports com.machinezoo.sourceafis.transparency.types;
