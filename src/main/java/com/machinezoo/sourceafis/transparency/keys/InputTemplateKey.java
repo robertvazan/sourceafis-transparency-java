711fda1b44502c825b8bd1874ec13de9c1e5aa1f
Robert Važan <robert.vazan@tutanota.com>
2022-01-23 09:44:01 +0000

    Fixed operation list for input template

--- src/main/java/com/machinezoo/sourceafis/transparency/keys/InputTemplateKey.java
+++ src/main/java/com/machinezoo/sourceafis/transparency/keys/InputTemplateKey.java
@@ -1,11 +1,17 @@
 // Part of SourceAFIS Transparency API: https://sourceafis.machinezoo.com/transparency/
 package com.machinezoo.sourceafis.transparency.keys;
 
+import java.util.*;
+import com.machinezoo.sourceafis.transparency.*;
 import com.machinezoo.sourceafis.transparency.types.*;
 
 public record InputTemplateKey() implements SideTemplateKey {
 	@Override
 	public MatchSide side() {
 		return null;
+	}
+	@Override
+	public List<TransparentOperation> operations() {
+		return List.of(TransparentOperation.PREPARE_PROBE, TransparentOperation.DESERIALIZE_TEMPLATE);
 	}
 }
