81546d8ed4bf290f5381519d8cef6aa416ba67d7
Robert Važan <robert.vazan@tutanota.com>
2022-10-08 22:08:42 +0000

    Cleanup in status notes

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,22 +1,23 @@
 // Part of SourceAFIS Transparency API: https://sourceafis.machinezoo.com/transparency/
 import com.machinezoo.stagean.*;
 
-@NoDocs
+@NoDocs("Add javadoc and website.")
 @DraftTests("Reach 100% coverage.")
 @DraftApi
 @ApiIssue("Allow keys with the same name that differ in supported MIME types.")
-/*
- * This is required to support multiple SourceAFIS versions:
- * 
- * - Key parsing requires version as parameter.
- * - TransparencyBuffer gets SourceAFIS version from VersionKey.
- * - Modified types and keys get version suffix (e.g. 3v14). Old types and keys are immediately deprecated.
- * - Deprecated types and keys are regularly removed when major version of this library is incremented.
- * - When unversioned symbol is removed, latest versioned symbol is copied to it and the versioned symbol is deprecated.
- * 
- * Unstable 0.x releases do not need to bother with compatibility. This can be delayed until release 1.0.
- */
-@ApiIssue("Allow keys with the same name that differ in SourceAFIS versions they apply to.")
+@ApiIssue("""
+	Allow keys with the same name that differ in SourceAFIS versions they apply to.
+	
+	This is required to support multiple SourceAFIS versions:
+	
+	- Key parsing requires version as parameter.
+	- TransparencyBuffer gets SourceAFIS version from VersionKey.
+	- Modified types and keys get version suffix (e.g. 3v14). Old types and keys are immediately deprecated.
+	- Deprecated types and keys are regularly removed when major version of this library is incremented.
+	- When unversioned symbol is removed, latest versioned symbol is copied to it and the versioned symbol is deprecated.
+	
+	Unstable 0.x releases do not need to bother with compatibility. This can be delayed until release 1.0.
+	""")
 module com.machinezoo.sourceafis.transparency {
 	exports com.machinezoo.sourceafis.transparency;
 	exports com.machinezoo.sourceafis.transparency.types;
