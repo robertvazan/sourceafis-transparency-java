1d333c09bf3dd0b92932ff547f1bfc3955867d0d
Robert Važan <robert.vazan@tutanota.com>
2023-04-08 21:58:22 +0000

    Workaround for javac bug

--- src/main/java/com/machinezoo/sourceafis/transparency/TransparencyKey.java
+++ src/main/java/com/machinezoo/sourceafis/transparency/TransparencyKey.java
@@ -8,6 +8,19 @@ import com.machinezoo.sourceafis.transparency.keys.*;
 import com.machinezoo.stagean.*;
 
 @ApiIssue("Add validation method, which is useful when type is an array.")
+@ApiIssue("""
+	Allow keys with the same name that differ in SourceAFIS versions they apply to.
+
+	This is required to support multiple SourceAFIS versions:
+
+	- Key parsing requires version as parameter.
+	- TransparencyBuffer gets SourceAFIS version from VersionKey.
+	- Modified types and keys get version suffix (e.g. 3v14). Old types and keys are immediately deprecated.
+	- Deprecated types and keys are regularly removed when major version of this library is incremented.
+	- When unversioned symbol is removed, latest versioned symbol is copied to it and the versioned symbol is deprecated.
+
+	Unstable 0.x releases do not need to bother with compatibility. This can be delayed until release 1.0.
+	""")
 public interface TransparencyKey<T> extends Serializable {
 	/*
 	 * Represents a group of keys that are variants of each other.
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,19 +5,6 @@ import com.machinezoo.stagean.*;
 @DraftTests("Reach 100% coverage.")
 @DraftApi
 @ApiIssue("Allow keys with the same name that differ in supported MIME types.")
-@ApiIssue("""
-	Allow keys with the same name that differ in SourceAFIS versions they apply to.
-	
-	This is required to support multiple SourceAFIS versions:
-	
-	- Key parsing requires version as parameter.
-	- TransparencyBuffer gets SourceAFIS version from VersionKey.
-	- Modified types and keys get version suffix (e.g. 3v14). Old types and keys are immediately deprecated.
-	- Deprecated types and keys are regularly removed when major version of this library is incremented.
-	- When unversioned symbol is removed, latest versioned symbol is copied to it and the versioned symbol is deprecated.
-	
-	Unstable 0.x releases do not need to bother with compatibility. This can be delayed until release 1.0.
-	""")
 module com.machinezoo.sourceafis.transparency {
 	exports com.machinezoo.sourceafis.transparency;
 	exports com.machinezoo.sourceafis.transparency.types;
